b7a9779b685a4bfcac0f2e13f3d72616a1ab7314
anshuman-111 <anshuman11197@gmail.com>
2022-12-22 18:14:26 +1100

    Alignment Issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
+
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,43 +4,43 @@ import {TfiEmail} from "react-icons/tfi";
 import { CgFileDocument } from 'react-icons/cg';
 
 const Contact = () => {
+
   const links = [ 
     { 
         id: 1,
         child: (<>
-            <FaLinkedin size={30}/>
+           <FaLinkedin size={30}/>LinkedIn 
             </>),
             href: "https://www.linkedin.com/in/anshumangupta1997/",
-            style: 'rounded-tr-md'
+ 
     },
     { 
         id: 2,
         child: (<>
-            <FaGithub size={30}/>
+            <FaGithub size={30}/>Github 
             </>),
             href: "https://github.com/anshuman-111",
     },
     { 
         id: 3,
         child: (<>
-            <TfiEmail size={30}/>
+           <TfiEmail size={30}/>Email 
             </>),
             href: "mailto:anshuman11197@gmail.com",
     },
     { 
         id: 4,
         child: (<>
-            <CgFileDocument size={30}/>
+            <CgFileDocument size={30}/>Resume 
             </>),
             href: "/resume.pdf",
-            style: 'rounded-br-md'
     },
 ]
 
   return (
     <div name="Contact" className='bg-gradient-to-b p-4 from-black to-slate-600 w-full h-screen text-white'>
         <div className='max-w-screen-lg mx-auto p-6 flex flex-col justify-center w-3/4 h-full rounded-2xl text-white bg-transparent'>
-        <div className='pb-8 text-center md:mt-20 mt-72'>
+        <div className='pb-8 text-center sm:mt-0 mt-56'>
             <p className='text-4xl mt-4 border-b-4 border-gray-500 font-bold inline'> Contact </p>
         </div>
         <div className='flex justify-center items-center'>
@@ -51,7 +51,7 @@ const Contact = () => {
                 placeholder='Enter your name'
                 name="name" 
                 id="name" 
-                className='p-2 bg-transparent border-2 rounded-md text-white foucs:outline-none'
+                className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
                 />
                 <input 
                 type="text" 
@@ -59,19 +59,20 @@ const Contact = () => {
                 placeholder='Enter your email'
                 name="email" 
                 id="email" 
-                className='p-2 my-5 bg-transparent border-2 rounded-md text-white foucs:outline-none'
+                className='p-2 my-5 bg-transparent border-2 rounded-md text-white focus:outline-none'
                 />
                 <textarea 
                 placeholder="Enter your message" 
                 type="text" 
                 name="message" 
                 id="message"
-                rows="8"
-                className='p-2 bg-transparent border-2 rounded-md text-white foucs:outline-none'
+                rows="7"
+                className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
                 />
-                <button className='bg-gradient-to-b from-blue-300 to-blue-500 rounded-lg px-8 py-3 mt-6 mx-auto flex items-center hover:scale-110 duration-300 hover:text-black' type="submit"> Submit </button>
+                <button className='bg-gradient-to-b from-blue-300 to-blue-500 rounded-lg px-8 py-3 mt-2 mx-auto flex items-center hover:scale-110 duration-300 hover:text-black' type="submit"> Submit </button>
             </form>
         </div>
+       
         </div>
     </div>
   )
