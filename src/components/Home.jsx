2b092cc4e6dfd77cb3d394fdfe66bebeef9719ba
anshuman-111 <anshuman11197@gmail.com>
2022-12-23 17:59:57 +1100

    Added extra information

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div name="Home" 
     className='p-4 w-full h-screen bg-gradient-to-b
-     from-black via-black to-slate-600'>
+     from-black via-black to-slate-700'>
         <div 
         className='max-w-screen-lg mx-auto flex flex-col 
         items-center justify-center h-full px-4 md:flex-row'>
@@ -16,14 +16,21 @@ const Home = () => {
 
                 <h2 
                 className='text-4xl sm:text-7xl font-bold text-white px-4 my-4'> 
-                Full-Stack Developer 
+                Software developer
                 </h2>
                 <h2 
-                className='text-4xl sm:text-7xl font-bold text-white px-4 my-4'> 
-                Data Analyst
+                className='text-3xl sm:text-7xl font-bold text-white px-4 my-4'> 
+                Aspiring Data Analyst
                 </h2>
                 <p 
-                className=' text-gray-400 py-4 max-w-md px-4'></p>
+                className=' text-gray-400 py-4 max-w-md px-4'>
+                Recent computer science graduate keen on building scalable web applications and analyzing data.
+                </p>
+                <p 
+                className=' text-gray-400 py-4 max-w-md px-4'>
+                    
+                </p>
+
 
                 <div className=' px-4'>
                     <Link 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,14 +20,14 @@ const Projects = () => {
       id: 2,
       src: fmda,
       alt: "Flatmates Data Web Scraper",
-      url: process.env.PUBLIC_URL + "/Flatmates.html",
+      url: "/Flatmates.html",
       giturl: "https://github.com/anshuman-111/flatmates-data"
     },
     {
       id: 3,
       src: sdc,
       alt: "Survey Dataset Cleaning",
-      url: process.env.PUBLIC_URL + "/survey_data_report.html",
+      url: "/survey_data_report.html",
       giturl: "https://github.com/anshuman-111/data-cleaning"
     }
   ]
@@ -35,7 +35,7 @@ const Projects = () => {
 
 
   return (
-    <div name="Projects" className=' bg-gradient-to-b from-slate-600 to-black w-full text-white p-4 md:h-screen'>
+    <div name="Projects" className=' bg-gradient-to-b from-slate-700 to-black w-full text-white p-4 md:h-screen'>
       <div className='my-5 max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Projects</p>
